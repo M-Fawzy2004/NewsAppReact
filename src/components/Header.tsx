2f996649342a82d811e7567d5d4fdac3a9650866
Mohamed Fawzy <mofawzy.com7@gmail.com>
2026-05-22 00:43:52 +0300

    Refactor Header component and add styles
    - Removed inline styles from Header component and created a separate Styles.ts file for better organization.
    - Updated Header component structure for improved layout and consistency.
    - Added new dependencies for expo-linear-gradient and react-native-reanimated in package.json and package-lock.json.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,22 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
+import styles from "./Styles";
 
 const Header = () => {
   return (
-    <View style={styles.container}>
-      <Ionicons name="person-circle" size={35} color="#000" />
-      <Text style={styles.textHeader}>News App</Text>
-      <Ionicons name="notifications-outline" size={30} color="#000" />
+    <View style={styles.containerHeader}>
+      <View style={styles.textContainer}>
+        <Ionicons name="person-circle" size={25} color="#000" />
+      </View>
+      <View style={styles.textHeader}>
+        <Text style={styles.textLabel}>News App</Text>
+      </View>{" "}
+      <View style={styles.textContainer}>
+        <Ionicons name="notifications-outline" size={25} color="#000" />
+      </View>
     </View>
   );
 };
 
 export default Header;
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: 10,
-  },
-  textHeader: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-});
---
+++ src/components/Styles.ts
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  containerHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 10,
+  },
+  textHeader: {
+    backgroundColor: "#eaeaea",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+    height: 55,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+  },
+  textLabel: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlignVertical: "center",
+  },
+  textContainer: {
+    backgroundColor: "#eaeaea",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+    height: 55,
+    width: 55,
+  },
+});
+
+export default styles;
